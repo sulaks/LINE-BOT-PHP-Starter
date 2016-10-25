054dab7c5a4aca8cb4e909fff3ab8e2146606291
sulaks <ssoommart@hotmail.com>
2016-10-25 21:04:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'] . ' นมน้อยจ๋า';
+			$text = $event['message']['text'] . ' ACM';
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
