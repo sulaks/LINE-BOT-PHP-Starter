5719aed7afc63ae889663265dcd5573f0fd395cb
sulaks <ssoommart@hotmail.com>
2017-01-09 16:31:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -43,7 +43,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'].$addMessage($text); 
+			$text = $event['message']['text'] . $addMessage(); 
 			
 			//$addMessage($text);
 			// Get replyToken
