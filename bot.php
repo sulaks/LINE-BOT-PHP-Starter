e5bce3ceb3c3ab812e45eefb2b85469b75af74b9
sulaks <ssoommart@hotmail.com>
2017-01-11 15:51:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -46,7 +46,7 @@ if (!is_null($events['events'])) {
 			//$text = $event['message']['text']; 
 			
 			//$addMessage($text);
-			$text = var_export($events);
+			$text = var_export($event);
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
