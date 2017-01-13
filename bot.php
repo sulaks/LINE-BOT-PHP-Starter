f6bff9254d146cf56ffbc3f51473e543cad0e337
sulaks <ssoommart@hotmail.com>
2017-01-13 10:53:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -57,6 +57,20 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+			
+			// Get profile
+			/*
+			$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+			$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+			$response = $bot->getProfile('<userId>');
+			if ($response->isSucceeded()) {
+			    $profile = $response->getJSONDecodedBody();
+			    echo $profile['displayName'];
+			    echo $profile['pictureUrl'];
+			    echo $profile['statusMessage'];
+			}
+			*/
+			////////////////
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
